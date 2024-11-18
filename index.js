cca538281c16b2db4e3d00a049227c4940fd716a
nikita_rayte <raytenikita7@gmail.com>
2024-11-18 10:01:39 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+
+app.use("/api/auth", require("./routes/auth.routes"))
+app.use("/api/todo", require("./routes/todo.routes"))
+
+app.use("*", (req, res) => {
+    res.status(404).json({ message: `route not found : ${req.method}:${req.url}` })
+})
+mongoose.connect(process.env.MONGO_URL)
+mongoose.connection.once("open", () => {
+    console.log("mongo connected")
+})
+app.listen(process.env.PORT, console.log("server running"))

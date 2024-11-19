c6ad9f261229c75c461e38afd0e78726383f56a4
nikita_rayte <raytenikita7@gmail.com>
2024-11-19 08:53:24 +0530

    frontend added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("dotenv").config()
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static("dist"))
 app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 
 app.use("/api/auth", require("./routes/auth.routes"))
